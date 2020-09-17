b78429622e00145aa8f3a765c148ea5b11f74e53
Matt Hamilton <matt@soluble.ai>
2020-09-17 13:40:46 -0400

    fix urls

--- cloud-init.sh
+++ cloud-init.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # download the CTF manifests
-curl -o /var/lib/rancher/k3s/server/manifests/ctf.yaml https://soluble-ai.github.io/k8s-tamu/static/cinderella-ctf/ctf.yaml
+curl -Lo /var/lib/rancher/k3s/server/manifests/ctf.yaml https://tamu.ctf.soluble.cloud/static/cinderella-ctf/ctf.yaml
 
 # set kubeconfig to default file
 su soluble -c -- "/usr/local/bin/kubectl config view --raw >> /home/soluble/.kube/config"
@@ -11,5 +11,5 @@ su soluble -c -- "/usr/local/bin/kubectl completion bash >> /home/soluble/.kube/
 su soluble -c -- "echo 'source /home/soluble/.kube/bash_completion.sh' >> /home/soluble/.bashrc"
 
 # download the manifest for 101/II - Workloads section
-curl -o /home/soluble/deployment.yaml https://soluble-ai.github.io/k8s-tamu/static/cindefella-ctf/deployment.yaml
+curl -Lo /home/soluble/deployment.yaml https://tamu.ctf.soluble.cloud/static/cindefella-ctf/deployment.yaml
 chmod 777 /home/soluble/deployment.yaml
